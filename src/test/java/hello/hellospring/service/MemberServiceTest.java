81834376b1c23a7691a72a30b58736749f29288b
Seungho <gemdoq@gmail.com>
2023-04-02 23:35:59 +0900

    Initial commit

---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,69 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.StatusResultMatchersExtensionsKt.isEqualTo;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    void beforeEach() throws Exception {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    void afterEach() throws Exception {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void 중복회원예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("hello");
+        Member member2 = new Member();
+        member2.setName("hello");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+
+    @Test
+    void 회원전체찾기() {
+    }
+
+    @Test
+    void 회원하나찾기() {
+    }
+}
